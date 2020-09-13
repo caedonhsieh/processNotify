5704d1aae4330a9bdfa38426c0d2a9324bb8d059
caedonhsieh <47953577+caedonhsieh@users.noreply.github.com>
2020-09-13 12:08:57 -0500

    Update setup.sh
    fixed typo

--- setup.sh
+++ setup.sh
@@ -17,6 +17,6 @@ chmod +x run_process.py
 
 # initialize processNotify.env
 echo TWILIO_ACCOUNT_SID=[INSERT ACCOUNT SID] > processNotify.env
-echo TWILIO_AUTH_TOKEN=[INSERT ACCOUNT SID] >> processNotify.env
+echo TWILIO_AUTH_TOKEN=[INSERT AUTH TOKEN] >> processNotify.env
 echo TWILIO_PHONE=+[TWILIO PHONE NUMBER] >> processNotify.env
 echo MY_PHONE=+[YOUR PHONE NUMBER] >> processNotify.env
